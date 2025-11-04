67f7c11ade66c3cbd7b39c25cbd7064cce94df34
christina <chrstnpet@gmail.com>
2025-11-05 00:27:23 +0200

    Implemented alert on submit capture and console display of the submitted information

--- JS/script.js
+++ JS/script.js
@@ -1,25 +1,56 @@
 // Import header and footer
-
 $(function() {
     $("#import-header").load("./header.html");
-});
-
-$(function() {
     $("#import-footer").load("./footer.html");
 });
 
-//----------------------------------------------------
-// API integration 
-
+//---------------------------------------------------------------
+// API integration
 const api_url = "http://api.quotable.io/random?tags=technology";
-const quote = document.querySelector('#quote');
-const author = document.querySelector('#author');
+const $quote = $('#quote');
+const $author = $('#author');
 
 async function getquote(url){
-    const response = await fetch(url);  
-    var data = await response.json();
-    quote.innerHTML = data.content;
-    author.innerHTML = data.author;
+    try {
+        const response = await fetch(url);  
+        var data = await response.json();
+        quote.innerHTML = data.content;
+        author.innerHTML = data.author;
+    }
+    catch(error) {
+        $quote.html("Could not fetch quote.");
+        $author.html("Could not fetch author");
+        console.error("Error fetching quote:", error);
+    }
 }
 
 getquote(api_url);
+
+//---------------------------------------------------------------
+// Collecting form information
+$(document).ready(function() {
+    $('#contact-us-form').on('submit', function (e) {
+        e.preventDefault();
+        
+        const formData = {
+            name: $('#sender-name').val(),
+            email: $('#sender-email').val(),
+            subject: $('#contact-subject').val(),
+            message: $('#message').val()
+        }
+
+        console.log('Contact Form Submission:', formData);
+
+        const alertMessage = 
+            `Thank you for your message! You've sent us the following information:\n\n` +
+            `Name: ${formData.name}\n` +
+            `Email: ${formData.email}\n` +
+            `Subject: ${formData.subject}\n` +
+            `Message: ${formData.message}\n` +
+            `We will reach out as soon as possible!`;
+
+        alert(alertMessage);
+
+        this.reset();
+    });
+});
