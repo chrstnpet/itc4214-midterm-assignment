9cd5c2cd334367e803540ec6e41c2f4ae4692795
christina <chrstnpet@gmail.com>
2025-11-04 02:01:59 +0200

    Integrated technology quotes API in Home page, added CSS style to its appearance and moved Logo-Font import to colorscheme file.

--- JS/script.js
+++ JS/script.js
@@ -9,3 +9,17 @@ $(function() {
 });
 
 //----------------------------------------------------
+// API integration 
+
+const api_url = "http://api.quotable.io/random?tags=technology";
+const quote = document.querySelector('#quote');
+const author = document.querySelector('#author');
+
+async function getquote(url){
+    const response = await fetch(url);  
+    var data = await response.json();
+    quote.innerHTML = data.content;
+    author.innerHTML = data.author;
+}
+
+getquote(api_url);
